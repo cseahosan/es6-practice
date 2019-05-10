12068281a3fea07e6cb38017b6f1ca59bc2aaf85
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-10 23:47:29 +0600

    let vs const vs var

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-const a = 4;
+// let vs  const vs var
 
-const sum = (a, b) => a + b;
+const a = 10;
 
-console.log(sum(a, 40))
-console.log('hello')
+console.log(a);
+
+// var a = 10;
+//
+// if(true){
+//     var a = 20;
+//     console.log(a);
+// }
+// console.log(a);
+//
+// let a = 10;
+//
+// if(true){
+//     let a = 20;
+//     console.log(a);
+// }
+// console.log(a);
