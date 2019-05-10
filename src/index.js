06d6ed316bc43162403fec9e6ede1c1852b43812
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-11 00:17:29 +0600

    enhance object

--- src/index.js
+++ src/index.js
@@ -1,36 +1,14 @@
-//problem: its giving output window object
+let name = 'Ahosan'
 
-// const obj = {
-//     name: 'Ahosan Habib',
-//     print: function () {
-//         setTimeout(function () {
-//             console.log(this)
-//         },1000)
-//     }
-// }
-//
-// obj.print();
+let email = 'mdahosanhabib@outlook.com'
 
-//solution 1
-// const obj = {
-//     name: 'Ahosan Habib',
-//     print: function () {
-//         setTimeout(function () {
-//             console.log(this)
-//         }.bind(this), 1000)
-//     }
-// }
-//
-// obj.print();
-
-// solution 2 (using arrow function)
 const obj = {
-    name: 'Ahosan Habib',
-    print: function () {
-        setTimeout(()=>{
-            console.log(this)
-        },1000)
+    name,
+    email,
+    print(){
+        console.log(this.name, this.email)
     }
 }
 
 obj.print();
+console.dir(obj)
