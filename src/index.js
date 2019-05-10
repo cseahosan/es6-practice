b3523e73c079afcb4138f0cafb64a805a0135138
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-11 00:29:26 +0600

    destructering array and object

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-let name = 'Ahosan'
+// const arr = [10, 20, 30]
+//
+// let [a, b, c] = arr;
+//
+// console.log(a)
+// console.log(b)
 
-let email = 'mdahosanhabib@outlook.com'
+// const obj = {
+//     name: 'Ahosan habib',
+//     email: 'mdahosanhabib@outlook.com'
+// }
+//
+// let {name, email} = obj;
+//
+// console.log(name)
 
-const obj = {
-    name,
-    email,
-    print(){
-        console.log(this.name, this.email)
-    }
-}
+// function print(name, email) {
+//     console.log(`Name: ${name} Email: ${email}`)
+// }
+//
+// print(name, email);
 
-obj.print();
-console.dir(obj)
+
+
+//
+// const obj = {
+//     name: 'Ahosan habib',
+//     email: 'mdahosanhabib@outlook.com'
+// }
+//
+// let {name, email} = obj;
+
+// no need ordering when pass parameter as object
+// function print({name, email}) {
+//     console.log(`Name: ${name} Email: ${email}`)
+// }
+//
+// print({email, name});
