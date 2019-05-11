6e0492224323b86e793968ac950edc5fdaf8de23
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-11 11:30:25 +0600

    module system (export, import)

--- src/index.js
+++ src/index.js
@@ -1,38 +1,4 @@
-// default parameter
-// let add = (a, b=0) => a+b
-// console.log(add(5))
+// import * as math from './math'; // for access all from math.js
+import {add, sub}  from './math'; // destructuring for access only add and sub method
 
-// Rest Operator Example 1
-// function sum(...num) {// convert value to array
-//     console.log(num)
-// }
-//
-// sum(10, 20)
-
-// Rest Operator Example 2
-// function sum(...num) {
-//     return num.reduce((a, b)=>a+b);
-// }
-//
-// console.log(sum(10, 20, 50))
-
-// Spread Operator Example 1
-// let arr = [1, 2, 3, 4];
-// console.log(...arr) // convert array to value
-
-// Spread Operator Example 2
-// let arr = [1, 2, 3, 4];
-// arr = [...arr, 5, 6, 7]
-// console.log(...arr) // convert array to value
-
-// Spread Operator Example 3
-let obj1 = {
-    name: 'Ahosan Habib',
-    email: 'mdahosanhabib@outlook.com'
-}
-
-let obj2 = {
-    ...obj1
-};
-
-console.log(obj2) // convert array to value
+console.log(add(1, 20))
---
+++ src/math.js
@@ -0,0 +1,5 @@
+export const add = (a, b) => a+b; //export use for make the method public
+
+export const sub = (a, b) => a-b;
+
+export const div = (a, b) => a/b;
