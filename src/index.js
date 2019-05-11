0ea11b5d7eae58e8eed0c76c282f010e238f7f81
Md Ahosan Habib <cseahosan@gmail.com>
2019-05-11 10:58:35 +0600

    default parameter, rest and spread operator

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
-// const arr = [10, 20, 30]
-//
-// let [a, b, c] = arr;
-//
-// console.log(a)
-// console.log(b)
+// default parameter
+// let add = (a, b=0) => a+b
+// console.log(add(5))
 
-// const obj = {
-//     name: 'Ahosan habib',
-//     email: 'mdahosanhabib@outlook.com'
+// Rest Operator Example 1
+// function sum(...num) {// convert value to array
+//     console.log(num)
 // }
 //
-// let {name, email} = obj;
-//
-// console.log(name)
+// sum(10, 20)
 
-// function print(name, email) {
-//     console.log(`Name: ${name} Email: ${email}`)
+// Rest Operator Example 2
+// function sum(...num) {
+//     return num.reduce((a, b)=>a+b);
 // }
 //
-// print(name, email);
+// console.log(sum(10, 20, 50))
 
+// Spread Operator Example 1
+// let arr = [1, 2, 3, 4];
+// console.log(...arr) // convert array to value
 
+// Spread Operator Example 2
+// let arr = [1, 2, 3, 4];
+// arr = [...arr, 5, 6, 7]
+// console.log(...arr) // convert array to value
 
-//
-// const obj = {
-//     name: 'Ahosan habib',
-//     email: 'mdahosanhabib@outlook.com'
-// }
-//
-// let {name, email} = obj;
+// Spread Operator Example 3
+let obj1 = {
+    name: 'Ahosan Habib',
+    email: 'mdahosanhabib@outlook.com'
+}
 
-// no need ordering when pass parameter as object
-// function print({name, email}) {
-//     console.log(`Name: ${name} Email: ${email}`)
-// }
-//
-// print({email, name});
+let obj2 = {
+    ...obj1
+};
+
+console.log(obj2) // convert array to value
